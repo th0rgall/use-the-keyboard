cdc4f133d6e6d9f8a5d9a4fcde301a8b275900d8
Thor Galle <thorgalle@gmail.com>
2024-08-26 12:17:13 +0300

    Makes browserSync work in a DevContainer

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
+let command = require('node-cmd');
+
 let mix = require('laravel-mix'),
     build = require('./taro.build.js');
 
@@ -9,12 +11,34 @@ mix.webpackConfig({
 });
 
 mix.setPublicPath('./')
-   .js('resources/assets/js/app.js', 'dist/assets/js')
-   .sass('resources/assets/sass/app.scss', 'dist/assets/css')
-   .copy('resources/assets/img/', 'dist/assets/img')
-   .options({
-       processCssUrls: false
-   })
-   .version();
+    .js('resources/assets/js/app.js', 'dist/assets/js')
+    .sass('resources/assets/sass/app.scss', 'dist/assets/css')
+    .copy('resources/assets/img/', 'dist/assets/img')
+    .options({
+        processCssUrls: false
+    })
+    .version();
 
-mix.browserSync();
+// To make this work in a devcontainer:
+// https://github.com/laravel-mix/laravel-mix/blob/b116bf064a7aa60dcc05cc9a03edbe9eb3a3ae2f/src/components/BrowserSync.js#L48
+// https://github.com/aschmelyun/taro/blob/98914cc2d376756327992034396227470b947bca/webpack.mix.js#L20
+// https://www.talvbansal.me/blog/browsersync-with-laravel-mix-and-docker/
+// https://stackoverflow.com/questions/42456424/browsersync-within-a-docker-container
+// https://github.com/laravel-mix/laravel-mix/blob/master/docs/browsersync.md
+mix.browserSync({
+    //host: "localhost",
+    proxy: null,
+    server: "dist",
+    port: 3000,
+    open: false,
+    files: [
+        {
+            match: ["content/*.json"],
+            fn: function (event, file) {
+                command.get('php taro build', (error, stdout, stderr) => {
+                    console.log(error ? stderr : stdout);
+                });
+            }
+        }
+    ]
+});
